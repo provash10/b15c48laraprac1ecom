a620b2f190c64d038caf79300959fb6b06622f93
provash10 <provash20cb@gmail.com>
2025-03-15 21:10:47 +0600

    67 l19 Backend Product Data Table CRUD3

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Color;
+use App\Models\GalleryImage;
 use App\Models\Product;
+use App\Models\Size;
 use App\Models\SubCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -25,6 +28,7 @@ class ProductController extends Controller
         if (isset($request->image)){
             $imageName = rand().'-product-'.'.'.$request->image->extension();
             $request->image->move('backend/images/product',$imageName);
+
             $product->image =$imageName;
         }
 
@@ -42,6 +46,52 @@ class ProductController extends Controller
         $product->product_type = $request->product_type;
 
         $product->save();
+
+        if(isset($request->color) && $request->color[0] != null){
+            foreach($request->color as $color_name){
+                $color = new Color();
+    
+                $color->product_id = $product->id;
+                // $color->color_name = $request->color;
+                $color->color_name = $color_name;
+
+                $color->save();
+            }
+        }
+
+        // $color->product_id = $product->id;
+        // $color->color_name = $request->color;
+
+        // Add Size 
+        if(isset($request->size) && $request->size[0] != null){
+            foreach($request->size as $size_name){
+                $size = new Size();
+    
+                $size->product_id = $product->id;
+                // $size->size_name = $request->size;
+                $size->size_name = $size_name;
+    
+                $size->save();
+            }
+
+            //Gallery Image
+            if(isset($request->galleryImage)){
+                foreach($request->galleryImage as $image){
+                    $galleryImage = new GalleryImage();
+
+                    $galleryImage->product_id =$product->id;
+
+                    $imageName = rand().'-galleryimage-'.'.'.$image->extension();
+                    $image->move('backend/images/galleryImage',$imageName);
+
+                    $galleryImage->image =$imageName;
+
+                    $galleryImage->save();
+
+                }
+            }
+        }
+        
         return redirect()->back();
     }
 }
